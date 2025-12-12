a49a52ba85c5e81e23a0eed7354aa59a4189f2db
Anh Ha <126641973+miaha1@users.noreply.github.com>
2025-12-12 17:16:17 -0500

    Refactor S3 event simulation in local test

--- test_local.py
+++ test_local.py
@@ -3,33 +3,31 @@ import json
 import logging
 import os
 import sys
-# This makes Python see 'utils' as if it were in the top folder
 sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 from app.llm_handler import lambda_handler
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Simulate S3 event (JSON structure)
+# Simulate S3 event
 fake_event = {
   "Records": [
     {
       "s3": {
         "bucket": {
-          "name": "health-assistant-data" # <--- CHECK YOUR BUCKET NAME
+          "name": "health-assistant-data" 
         },
         "object": {
-          "key": "inputs/test.txt"        # <--- Make sure this file exists in S3
+          "key": "inputs/test.txt"        
         }
       }
     }
   ]
 }
 
-print("--- 🚀 Starting Local Test ---")
+print(" Starting Local Test")
 response = lambda_handler(fake_event, None)
 
-# 3. Print the result
-print("\n--- ✅ Result ---")
+print("\n Result ")
 print(json.dumps(response, indent=2))
 
